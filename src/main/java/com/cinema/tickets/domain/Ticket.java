7425298b37f3d9db7c54cb4d42d3900d4feae4d9
kbledzki <krzysztofbledzki@gmail.com>
2023-11-06 13:07:54 +0100

    kb_12_tickets_refactor

--- src/main/java/com/cinema/tickets/domain/Ticket.java
+++ src/main/java/com/cinema/tickets/domain/Ticket.java
@@ -1,7 +1,13 @@
 package com.cinema.tickets.domain;
 
 import com.cinema.tickets.domain.exceptions.TicketAlreadyCancelledException;
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.ToString;
 
